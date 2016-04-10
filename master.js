66395089ac57ae774988c4b810cdbd57b5f38207
zmenka <mar4.evg@gmail.com>
2016-04-10 23:35:57 +0500

    new func

--- master.js
+++ master.js
@@ -10,48 +10,166 @@ var errors = require('./errors');
 
 module.exports = Master;
 
-function Master(serialPort, onReady) {
+function Master(serialPort) {
     var self = this;
-    serialPort.on('error', function(err){
-        throw err;
-    });
-    this.serial = new SerialHelper(serialPort, function(){
-        if (onReady)
-            onReady(self);
-    });
+
+    this.queue = [];
+    this.currentTask = null;
+
+    this.serialPort = serialPort;
+
+    return new Promise(function(resolve, reject){
+        self.serialPort.on('error', function(err){
+            reject(err);
+        });
+
+        self.serialPort.on('close', function(err){
+            reject(err);
+        })
+
+        self.serialPort.on('disconnect', function(err){
+            reject(err);
+        })
+
+        self.serialPort.on("open", function () {
+            self.processQueue();
+            resolve(self)
+        });
+
+        serialPort.on('data', function (buffer) {
+            self.doTask(buffer)
+        });
+    })
 }
 
-/**
- * Modbus function read holding registers
- * @param slave
- * @param start
- * @param length
- * @returns {promise}
- */
-Master.prototype.readHoldingRegisters = function (slave, start, length) {
-    var packet = this.createFixedPacket(slave, constants.FUNCTION_CODES.READ_HOLDING_REGISTERS, start, length);
+Master.prototype.doTask (buffer){
+    var self = this;
+    if (self.currentTask) {
+        constants.DEBUG && console.log('resp', buffer);
 
-    return this.request(packet)
-        .then(function (buffer) {
-            var data = binary.parse(buffer.slice(2, buffer.length - 2)); //slice header and crc
-            var results = [];
+        try {
+            if (buffer.length < 5){
+                throw new Error('Small buffer length' + buffer.length);
+            }
+            if ( !self.checkCrc()){
+                throw new errors.crc
+            }
+            if ( !self.validateRequest(buffer, self.currentTask.slave, self.currentTask.func, self.currentTask.len)){
+                throw new Error('Not valid packet for slave ' + self.currentTask.slave + ', func ' + self.currentTask.func + ', len ' + elf.currentTask.len);
+            }
 
-            data.word8('byteCount').tap(function (val) {
-                this.buffer('value', val.byteCount).tap(function () {
-                    for (var i = 0; i < val.byteCount; i += 2) {
-                        results.push(val.value.readInt16BE(i));
-                    }
-                });
+            self.currentTask.deferred.resolve(buffer);
+        } catch (err){
+            self.currentTask.errors.push(err)
+            self.currentTask.errCount++;
+            if (self.currentTask.errCount > constants.TASK_RETRY_COUNT){
+                self.currentTask.errors.push(new Error('Too much errors with this task'));
+                return self.currentTask.deferred.reject(self.currentTask.errors);
+            }
+        }
+    }
+}
+
+Master.prototype.write = function(buffer, deferred, simple) {
+    constants.DEBUG && console.log('write', buffer);
+    this.serialPort.write(buffer, function (error) {
+        if (error)
+            return deferred.reject(error);
+        if (simple)
+            return deferred.resolve();
+
+    });
+
+    return deferred.promise.timeout(constants.RESPONSE_TIMEOUT, 'Response timeout exceed!');
+};
+
+Master.prototype.processQueue = function () {
+    var self = this;
+
+    function continueQueue() {
+        setTimeout(function(){
+            self.processQueue();
+        }, constants.QUEUE_TIMEOUT) //pause between calls
+    }
+
+    if (this.queue.length) {
+        this.currentTask = self.queue.shift();
+        this.write(this.currentTask.buffer, this.currentTask.deferred, !this.currentTask.slave && !this.currentTask.func && !this.currentTask.len)
+            .catch(function(err){
+                self.currentTask.deferred.reject(err)
             })
+            .finally(function () {
+                continueQueue();
+            }).done();
+    } else {
+        continueQueue();
+    }
+};
 
-            return results;
-        });
+Master.prototype.request = function request(buffer, slave, func, len) {
+    var self = this;
+
+    var deferred = {};
+
+    deferred.promise = new Promise(function (resolve, reject) {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+
+
+    this.queue.push({
+        deferred: deferred,
+        buffer: buffer,
+        slave: slave,
+        func: func,
+        len: len,
+        errors: [],
+        errCount: 0
+    })
+
+    return deferred.promise;
+}
+
+Master.prototype.addCrc = function (buffer) {
+    return (new BufferPut())
+        .put(buffer)
+        .word16le(crc.crc16modbus(buffer))
+        .buffer();
+}
+
+Master.prototype.validateRequest = function (buffer, slave, func, len) {
+    var vars = binary.parse(buffer)
+        .word8be('slave')
+        .word8be('func')
+        .word8be('len')
+        .vars;
+    if (slave && vars.slave != slave){
+        return false
+    } else if (func && func!=vars.func){
+        return false;
+    } else if (len && len!=vars.len){
+        return false;
+    }
+    return true;
+}
+
+Master.prototype.checkCrc = function (buffer){
+    var pdu = buffer.slice(0, buffer.length-2);
+    return bufferEqual(buffer, this.addCrc(pdu));
+};
+
+Master.prototype.readHoldingRegisters = function (slave, start, length) {
+    return this.readRegisters(constants.FUNCTION_CODES.READ_HOLDING_REGISTERS, slave, start, length)
 }
 
 Master.prototype.readInputRegisters = function (slave, start, length) {
-    var packet = this.createFixedPacket(slave, 0x04, start, length);
+    return this.readRegisters(constants.FUNCTION_CODES.READ_INPUT_REGISTERS, slave, start, length)
+}
 
-    return this.request(packet)
+Master.prototype.readRegisters = function (func, slave, start, length) {
+    var packet = this.createFixedPacket(slave, func, start, length);
+
+    return this.request(packet, slave, start, length)
         .then(function (buffer) {
             var data = binary.parse(buffer.slice(2, buffer.length - 2)); //slice header and crc
             var results = [];
@@ -69,9 +187,9 @@ Master.prototype.readInputRegisters = function (slave, start, length) {
 }
 
 Master.prototype.read19 = function (slave, start) {
-    var packet = this.createFixedPacketSmall(slave, 0x19, start);
+    var packet = this.createFixedPacketSmall(slave, constants.FUNCTION_CODES.READ_ONE_REGISTER, start);
 
-    return this.request(packet)
+    return this.request(packet, slave, start)
         .then(function (buffer) {
             //var data = binary.parse(buffer.slice(2, buffer.length - 2)); //slice header and crc
             //var results = [];
@@ -86,12 +204,9 @@ Master.prototype.read19 = function (slave, start) {
 }
 
 Master.prototype.startRead = function (slave, start, length) {
-    var packet = this.createFixedPacket(slave, 0x08, start, length);
+    var packet = this.createFixedPacket(slave, constants.FUNCTION_CODES.START_READ, start, length);
 
     return this.request(packet)
-        .then(function (buffer) {
-            return [];
-        });
 }
 
 Master.prototype.writeSingleRegister = function (slave, register, value, retryCount) {
@@ -130,37 +245,24 @@ Master.prototype.writeMultipleRegisters = function(slave, start, array){
     return this.request(packet);
 }
 
-/**
- * Create modbus packet with fixed length
- * @param slave
- * @param func
- * @param param
- * @param param2
- * @returns {*}
- */
 Master.prototype.createFixedPacket = function(slave, func, param, param2){
-   return (new BufferPut())
+   return this.addCrc((new BufferPut())
         .word8be(slave)
         .word8be(func)
         .word16be(param)
         .word16be(param2)
-        .buffer();
+        .buffer());
 }
 
 Master.prototype.createFixedPacketSmall = function(slave, func, param){
-    return (new BufferPut())
+    return this.addCrc((new BufferPut())
         .word8be(slave)
         .word8be(func)
         .word16be(param)
-        .buffer();
+        .buffer());
 }
-/**
- * Create modbus packet with various length
- * @param slave
- * @param func
- * @param start
- * @param array
- */
+
+
 Master.prototype.createVariousPacket = function(slave, func, start, array){
     var buf = (new BufferPut())
         .word8be(slave)
@@ -173,38 +275,8 @@ Master.prototype.createVariousPacket = function(slave, func, start, array){
         buf.word16be(value);
     })
 
-    return buf.buffer();
+    return this.addCrc(buf.buffer());
 }
 
-Master.prototype.request = function request(buffer) {
-    var self = this;
 
-    return this.serial.write(this.addCrc(buffer))
-        .then(function (response) {
-            if (!self.checkCrc(response))
-                throw new errors.crc;
-            return response;
-        })
-}
-
-Master.prototype.addCrc = function (buffer) {
-    return (new BufferPut())
-        .put(buffer)
-        .word16le(crc.crc16modbus(buffer))
-        .buffer();
-}
-
-Master.prototype.validateRequest = function (buffer, slave, func) {
-    var vars = binary.parse(buffer)
-        .word8be('slave')
-        .word8be('func')
-        .vars;
-
-    return vars.slave == slave && vars.func == func;
-}
-
-Master.prototype.checkCrc = function (buffer){
-    var pdu = buffer.slice(0, buffer.length-2);
-    return bufferEqual(buffer, this.addCrc(pdu));
-};
 
